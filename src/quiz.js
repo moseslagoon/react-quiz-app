70521a38f1be1430c92cd8e4f81fc8635c16893b
Moses Lagoon <mxl2261@g.rit.edu>
2019-02-25 00:04:23 -0500

    first commit

---
+++ src/quiz.js
@@ -0,0 +1,35 @@
+// import React from 'react';
+// import Quiz from 'react-quiz-component';
+
+export const quiz =  {
+  "quizTitle": "US Citizenship Civics Test",
+  "quizSynopsis": "Civics Test for Naturalization ",
+  "questions": [
+    {
+      "question": "What is the supreme law of the land? ",
+      "questionType": "text",
+      "answers": [
+        "The Constitution",
+        "Blah",
+        "Blah",
+        "Blah"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct!",
+      "messageForInorrectAnswer": "Incorrect!"
+    },
+    {
+      "question": "What does the Constitution do?",
+      "questionType": "text",
+      "answers": [
+        "sets up the government ",
+        "defines the government",
+        "protects basic rights of Americans",
+        "All of the above"
+      ],
+      "correctAnswer": "4",
+      "messageForCorrectAnswer": "Correct!",
+      "messageForInorrectAnswer": "Incorrect!"
+    }
+  ]
+}
